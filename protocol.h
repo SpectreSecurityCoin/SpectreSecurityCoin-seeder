1b40b8bcca2ed17ce7ecc19f91c750095a3e87d2
SOELexicon <craig_wright156@hotmail.com>
2018-08-28 05:12:10 +0100

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 17778 : 28192;
+    return testnet ? 17778 : 13338;
 }
 
 //

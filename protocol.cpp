5843b01ce61e8d9364f69063f6fb0cd38a29f4ef
SOELexicon <craig_wright156@hotmail.com>
2018-08-28 05:11:16 +0100

    Update protocol.cpp

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xcd, 0xf2, 0xc0, 0xef };
+unsigned char pchMessageStart[4] = { 0xe5, 0x5a, 0xc4, 0x54 };
 
 CMessageHeader::CMessageHeader()
 {
